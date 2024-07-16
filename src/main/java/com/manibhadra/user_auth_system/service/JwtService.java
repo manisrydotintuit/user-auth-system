0992746c10aa0d87c3d5174c71bd13d0513da94b
manisrydotintuit <manis@rydotintuit.com>
2024-07-16 15:05:14 +0530

    little change

--- src/main/java/com/manibhadra/user_auth_system/service/JwtService.java
+++ src/main/java/com/manibhadra/user_auth_system/service/JwtService.java
@@ -41,8 +41,6 @@ public class JwtService {
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(username)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
                 .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
